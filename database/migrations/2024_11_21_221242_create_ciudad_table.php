9f80eb5c7e6c908133b21b585b2989a809d174a3
LucasWalica <lucasw58@educastur.es>
2024-11-21 23:38:02 +0100

    migrations added

---
+++ database/migrations/2024_11_21_221242_create_ciudad_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ciudad', function (Blueprint $table) {
+            $table->id();
+           $table->string('name');
+           $table->foreignId('partida_id')->constrained('partida')->onDelete('cascade');
+           $table->boolean('centro_investigacion')->default(false);
+           $table->integer('eVerde');
+           $table->integer('eRoja');
+           $table->integer('eAmarilla');
+           $table->integer('eAzul');
+           $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ciudad');
+    }
+};
---
+++ database/migrations/2024_11_21_222046_create_personaje__table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personaje_', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('partida_id')->constrained('partida')->onDelete('cascade');
+            $table->boolean('movido')->default(false);
+            $table->foreignId('ciudad_id')->constrained('ciudad')->onDelete('cascade'); // Cambiado de 'id' a 'ciudad_id'
+            $table->integer('turno_comienzo')->default(0);
+            $table->boolean('en_accion')->default(false);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personaje_');
+    }
+};
