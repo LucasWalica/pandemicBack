737a94aeaae1b27927dfe4939920a4461b0a5f70
LucasWalica <lucasw58@educastur.es>
2024-11-27 20:14:58 +0100

    minnor changes

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\PartidaController;
 
-
 Route::group(['middleware' => 'api'], function () {
+
     Route::prefix('partidas')->group(function () {
         Route::post('', [PartidaController::class,'store']);
         Route::get('/{id}', [PartidaController::class,'show']);
@@ -21,7 +20,8 @@ Route::group(['middleware' => 'api'], function () {
     });
 
     Route::prefix('auth')->group(function () {
-        Route::post('register', [RegisteredUserController::class, 'store']);  // Asegúrate de que sea POST
-        Route::post('login', [AuthenticatedSessionController::class, 'store']);  // Asegúrate de que sea POST
+        Route::post('register', [RegisteredUserController::class, 'store']);  
+        Route::post('login', [AuthenticatedSessionController::class, 'store']);  
     });
+
 });
